249d044adc26dcf708acbb4126d52f5ae8977cd0
manishamaidam <maidammanisha.gmail.com>
2025-06-19 00:11:41 +0530

    shared lib

---
+++ vars/pipelineGlobals.groovy
@@ -0,0 +1,9 @@
+static final String region(){
+    return "us-east-1"
+}
+static final String nexusURL(){
+    return "nexus.manisha97.site:8081"
+}
+static final String account_id(){
+    return "535002850032"
+}
