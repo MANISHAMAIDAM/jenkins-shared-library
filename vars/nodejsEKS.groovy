5c9e44e5d7ab75b53ede1e8636aa1fa13aa514b0
manishamaidam <maidammanisha.gmail.com>
2025-06-19 00:28:09 +0530

    shared lib

--- vars/nodejsEKS.groovy
+++ vars/nodejsEKS.groovy
@@ -15,7 +15,7 @@ def call(Map configMap){
             account_id = pipelineGlobals.account_id()
             component = configMap.get("component")
             project = configMap.get("project")
-   
+
         }
         stages {
             stage('read the version'){
@@ -60,25 +60,12 @@ def call(Map configMap){
             stage('Deploy'){
                 steps{
                     script{
-                        releaseExists = sh(script: "helm list -A --short | grep -w ${component} || true", returnStdout: true).trim()
-                        if(releaseExists.isEmpty()){
-                            echo "${component} not installed yet, first time installation"
                             sh"""
                                 aws eks update-kubeconfig --region ${region} --name ${project}-dev
                                 cd helm
                                 sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
                                 helm install ${component} -n ${project} .
-                            """
-                        }
-                        else{
-                            echo "${component} exists, running upgrade"
-                            sh"""
-                                aws eks update-kubeconfig --region ${region} --name ${project}-dev
-                                cd helm
-                                sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
-                                helm upgrade ${component} -n ${project} .
-                            """
-                        }
+                            """                     
                     }
                 }
             }
